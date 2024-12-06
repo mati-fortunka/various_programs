add17c1d3b3c051b30fc8b42d42f15498dd2a380
mati-fortunka <mfortunka@student.uw.edu.pl>
2024-12-06 20:30:59 +0000

    programs added

--- degradation/fit_KM.py
+++ degradation/fit_KM.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 # Load data from CSV
-data = np.loadtxt('dane.csv', delimiter=';', unpack=True)
+data = np.loadtxt('dane_trmd.csv', delimiter=',', unpack=True)
 
 # Define the degradation function
 def f(x, V, Km):
@@ -20,8 +20,13 @@ y_data = data[1]
 yerr_data = data[2]
 popt, pcov = curve_fit(f, x_data, y_data, p0=[V_init, Km_init], sigma=yerr_data)
 
-# Extract fitted parameters
+# Extract fitted parameters and their errors
 V_fit, Km_fit = popt
+V_err, Km_err = np.sqrt(np.diag(pcov))
+
+# Print final Km, V_max, and their errors
+print(f"Final Km: {Km_fit} ± {Km_err}")
+print(f"Final V_max: {V_fit} ± {V_err}")
 
 # Create plot
 plt.figure(figsize=(8, 6))
@@ -43,8 +48,8 @@ plt.axhline(y=Vmax, color="#E61C66", linestyle='--', lw=1.5)
 
 # Set titles and labels
 plt.title('Degradation')
-plt.xlabel('[TrmD-ssra] (µM)')
-plt.ylabel('Degradation rate (µM/min)')
+plt.xlabel('[TrmD-ssra] (uM)')
+plt.ylabel('Degradation rate (uM/min)')
 plt.xlim(0, 60)
 plt.ylim(0, 1.2)
 
---
+++ degradation/plot_Tm1570.plt
@@ -0,0 +1,38 @@
+set datafile separator ','
+
+set terminal pngcairo size 1366,768 enhanced font 'Calibri,14'
+set output 'Tm1570.png'
+
+# style linii
+set style line 1 lc rgb "#000000" lw 2 pt 7
+set style line 2 lc rgb "#0000EE" lw 2 pt 7
+set style line 3 lc rgb "#E61C66" lw 1.5 pt 7 dt 3
+
+# definicja fitowanej funkcji i poczatkowe parametry
+f(x)=V*x/(x+Km)
+V = 0.9
+Km = 4.1
+Vmax = 0.99
+
+# rozne tam
+set title 'Tm1570-ssrA degradation'
+set xlabel '[Tm1570-ssra] (uM)'
+set ylabel 'Degradation rate (uM/min)'
+set xrange [0:25]
+set yrange [0:1.2]
+
+# kreski dla Km, Vmax/2
+set arrow from 4.12,0 to 4.12,0.495 nohead ls 3 
+set arrow from 0,0.495 to 4.12,0.495 nohead ls 3 
+
+# dopasowanie danych
+fit f(x) "dane_Tm1570.csv" u 1:2 via V, Km
+#fit f(x) "dane_Tm1570.csv" u 1:2:3 via V, Km
+
+# pozycja legendy
+set key right top
+#unset key -- jesli ma byc bez legendy
+
+# rysowanie
+plot "dane_Tm1570.csv" u 1:2:3 with yerrorbars title "Data" ls 2, f(x) title "Fit" ls 1, Vmax ls 3 notitle
+pause -1
